8db5be68a7d711fd569b8ffa2232bbaca5177243
Jonne Tanninen <jonnetanninen>
2020-09-22 15:40:32 +0300

    Add structure for apartment card

--- src/modules/search/ApartmentCard.tsx
+++ src/modules/search/ApartmentCard.tsx
@@ -5,11 +5,28 @@ import css from './ApartmentCard.module.css';
 
 const ApartmentCard = () => {
   return (
-    <div className={css.App}>
-      <div>Search parameters here</div>
-      <div>Notification here?</div>
-      <div>Search results or map here</div>
-      <div>Free apartments or map here</div>
+    <div className={css.container}>
+      <img src={'http://placekitten.com/400/250'} alt='Kuva asunnosta' />
+      <div className={css.content}>
+        <div className={css.info}>
+          <div className={css.price}>
+            vh.
+            <b> 329 875 €</b>
+          </div>
+          <div className={css.size}><b>85&#13217;</b></div>
+        </div>
+        <div className={css.details}>
+          <div className={css.apartmentDetails}>
+            <div>A1 | 1.kerros</div>
+            <div>4h + kt + s</div>
+          </div>
+          <div>Vähän hakijoita</div>
+        </div>
+        <div className={css.controls}>
+          <button>Tutustu huoneistoon</button>
+          <button>Luo hakemus</button>
+        </div>
+      </div>
     </div>
   );
 }
