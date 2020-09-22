842cfe741b6fb30a7eafc505a93887282bfea2cf
Jonne Tanninen <jonnetanninen>
2020-09-22 14:18:04 +0300

    Use css modules

--- src/modules/search/SearchContainer.tsx
+++ src/modules/search/SearchContainer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './SearchContainer.css';
+import css from './SearchContainer.module.css';
 
 function SearchContainer() {
   return (
-    <div className="container">
+    <div className={css.App}>
       <div>Search parameters here</div>
       <div>Notification here?</div>
       <div>Search results or map here</div>
