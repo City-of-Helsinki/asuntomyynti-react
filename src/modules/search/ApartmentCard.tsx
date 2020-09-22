5a0b058a5ba2993f01f14b711ef417054aeaccd0
Jonne Tanninen <jonnetanninen>
2020-09-22 14:37:03 +0300

    Add ApartmentCard and use arrow functions

--- src/modules/search/ApartmentCard.tsx
+++ src/modules/search/ApartmentCard.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import css from './SearchContainer.module.css';
-import ApartmentCard from "./ApartmentCard";
+import css from './ApartmentCard.module.css';
 
-const SearchContainer = () => {
+
+
+const ApartmentCard = () => {
   return (
     <div className={css.App}>
       <div>Search parameters here</div>
       <div>Notification here?</div>
       <div>Search results or map here</div>
       <div>Free apartments or map here</div>
-      <ApartmentCard />
     </div>
   );
 }
 
-export default SearchContainer;
+export default ApartmentCard;
