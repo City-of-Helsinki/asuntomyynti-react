f51c51c02e74ad1f043ccc777dca29d786f1bd35
Erik Rekola <erik.rekola@digia.com>
2020-09-30 14:56:49 +0300

    Add useConfig hook for fetching search configuration

---
+++ src/hooks/useConfig.ts
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import mockSearchConfig from './mocks/search-config.json';
+
+export type SearchItem = {
+  label: string;
+  placeholder?: string;
+  max?: number;
+  min?: number;
+  info?: string;
+};
+
+export type SearchConfig = {
+  items: (string | SearchItem)[];
+  label: string;
+};
+
+const defaultConfig = {
+  items: [],
+  label: '',
+  range: null,
+  max: null,
+};
+
+const fetchMockConfig = (name: string): Promise<SearchConfig> =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        ...defaultConfig,
+        ...(mockSearchConfig as { [key: string]: SearchConfig })[name], // HAXOR for the mock
+      });
+    }, 1000);
+  });
+
+const useConfig = (name: string) => {
+  const [config, setConfig] = useState<SearchConfig>(defaultConfig);
+
+  useEffect(() => {
+    fetchMockConfig(name).then((config) => setConfig(config));
+  });
+
+  return config;
+};
+
+export default useConfig;
