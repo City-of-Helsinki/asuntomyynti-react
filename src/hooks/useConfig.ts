6b8dca800a63fa704278c1db32226d0e880732a2
Erik Rekola <erik.rekola@digia.com>
2020-09-30 15:04:44 +0300

    Clean out React import

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import mockSearchConfig from './mocks/search-config.json';
 
 export type SearchItem = {
