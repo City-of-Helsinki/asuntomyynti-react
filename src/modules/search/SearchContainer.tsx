b4c2f0ecbe57378c440b8a95bda1a760ea74a039
Jonne Tanninen <jonnetanninen>
2020-09-30 13:43:17 +0300

    Fetch live development search data

--- src/modules/search/SearchContainer.tsx
+++ src/modules/search/SearchContainer.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import css from './SearchContainer.module.scss';
 import ProjectCard from './ProjectCard';
 import SearchResults from './SearchResults';
+import axios from 'axios';
 
 const SearchContainer = () => {
+  const [searchResults, setSearchResults] = useState();
+
+  useEffect(() => {
+
+    const fetchProjects = async () => {
+      try {
+        const data = await axios.get('http://dev.asuntomyynti-elastic.druidfi.wod.by/_search');
+        setSearchResults(data?.data?.hits?.hits.map((x: any) => x._source) || []);
+      } catch (e) {
+        // TODO
+      }
+    };
+    fetchProjects();
+
+  }, []);
+
+  console.warn(searchResults);
+
   return (
     <div className={css.App}>
       <div>Search parameters here</div>
