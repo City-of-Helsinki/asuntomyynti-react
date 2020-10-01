f1ace51f3005c6f9c5a50841073a90a6ba9aa565
Jonne Tanninen <jonnetanninen>
2020-10-01 14:21:00 +0300

    Add localization

--- src/modules/search/SearchResults.tsx
+++ src/modules/search/SearchResults.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import css from './SearchResults.module.scss';
 import ProjectCard from './ProjectCard';
 import { Project } from '../../types/common';
+import {useTranslation} from "react-i18next";
 
 type Props = {
   searchResults: Project[];
 };
 
 const SearchResults = ({ searchResults }: Props) => {
+  const {t} = useTranslation();
+
   return (
     <div className={css.container}>
       <div className={css.titleContainer}>
         <h1>Vapaat asunnot</h1>
+        <h2>{t('SEARCH:Welcome to React')}</h2>
         <div className={css.resultsCount}>Yhteensä {searchResults.length} huoneistoa</div>
       </div>
       <div className={css.resultWrapper}>
