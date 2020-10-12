faccda78392f141c2576237b43cd9ed25cacfed1
Erik Rekola <erik.rekola@digia.com>
2020-10-12 14:23:28 +0300

    Separate search params state to a hook Update useSearchParams

---
+++ src/hooks/useSearchParams.ts
@@ -0,0 +1,61 @@
+import useQuery from './useQuery';
+import { getValueByNameFromParams } from '../utils/search';
+import { useHistory } from 'react-router-dom';
+
+const useSearchParams = () => {
+  const query = useQuery();
+  const history = useHistory();
+
+  const getAllValues = () => {
+    return Array.from(query.entries()).reduce((all: { [name: string]: string }[], [name, values]) => {
+      const items = values
+        .split(',')
+        .filter((value) => value !== '') // Don't render empty values
+        .map((value) => ({ name, value }));
+
+      return [...all, ...items];
+    }, []);
+  };
+
+  const getValues = (name: string) => {
+    return getValueByNameFromParams(query, name, []);
+  };
+
+  const getValue = (name: string) => {
+    return query.get(name);
+  };
+
+  const setValue = (name: string, value: string) => {
+    if (value === '') {
+      query.delete(name);
+    } else {
+      query.set(name, value);
+    }
+    history.push(`?${query.toString()}`);
+  };
+
+  const setValues = (name: string, values: string[]) => {
+    setValue(name, values.join(','));
+  };
+
+  const addValue = (name: string, value: string) => {
+    const values = getValueByNameFromParams(query, name, []);
+    values.push(value);
+    setValue(name, values.join(','));
+  };
+
+  const removeValue = (name: string, value: string) => {
+    const values = getValueByNameFromParams(query, name, []);
+    const filteredValues = values.filter((x: string) => x !== value);
+    setValue(name, filteredValues.join(','));
+  };
+
+  const clearValues = (name: string) => {
+    query.delete(name);
+    history.push(`?${query.toString()}`);
+  };
+
+  return { getAllValues, getValues, getValue, addValue, setValue, setValues, removeValue, clearValues };
+};
+
+export default useSearchParams;
