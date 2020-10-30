c648ff56b1bda3f3b64db42b9b7c419a19405272
Jonne Tanninen <jonnetanninen>
2020-10-30 11:01:31 +0200

    Add marker

--- src/modules/search/components/result/MapContainer.tsx
+++ src/modules/search/components/result/MapContainer.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import css from './SearchResults.module.scss';
 import { Project } from '../../../../types/common';
 import { useTranslation } from 'react-i18next';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { IconLocation } from "hds-react";
+import L from 'leaflet';
 
 type Props = {
   searchResults: Project[];
@@ -11,20 +14,28 @@ type Props = {
 const MapContainer = ({ searchResults }: Props) => {
   const { t } = useTranslation();
   const sami = {
-    lat: 51,
-    lng: -1,
+    lat: 60.2,
+    lng: 24.92,
     zoom: 13,
   };
   const position = [sami.lat, sami.lng];
 
+  const icon = L.divIcon({
+    className: 'custom-icon',
+    html: ReactDOMServer.renderToString(<IconLocation size={'l'} />)
+  });
+
   return (
     <div className={css.container}>
       <div id={'mapid'}>
-        <Map center={[51, 0]} zoom={13}>
+        <Map center={[60.2, 24.92]} zoom={13} maxBounds={[[59.4, 23.8], [61.5, 25.8]]}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://tiles.hel.ninja/styles/hel-osm-bright/{z}/{x}/{y}.png"
           />
+          {
+            searchResults.map(x => <Marker icon={icon} position={[60.2, 24.92]} />)
+          }
         </Map>
       </div>
     </div>
