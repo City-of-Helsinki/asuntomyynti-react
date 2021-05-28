bb5124ecf73c6e245f373a7b397a37a748451094
Juha Kujala <juha.kujala@anders.fi>
2021-05-28 13:04:06 +0300

    Enhance accessibility for search results as a map

--- src/modules/search/components/result/map/MapProjectPopupCard.tsx
+++ src/modules/search/components/result/map/MapProjectPopupCard.tsx
@@ -13,6 +13,7 @@ type Props = {
   project: Project;
   currentLang: string;
   hideApartments?: boolean;
+  activeProject: Project | undefined;
   onCloseBtnClick: () => void;
   onApartmentsBtnClick: () => void;
 };
@@ -21,28 +22,38 @@ const MapProjectPopupCard = ({
   project,
   currentLang,
   hideApartments = false,
+  activeProject,
   onCloseBtnClick,
   onApartmentsBtnClick,
 }: Props) => {
   const { t } = useTranslation();
-  const { apartments, housing_company, main_image_url, ownership_type, url, state_of_sale } = project;
+  const { apartments, id, housing_company, main_image_url, ownership_type, url, state_of_sale } = project;
   const filteredApartments = getLanguageFilteredApartments(apartments, currentLang);
   const hasApartments = !!filteredApartments.length;
 
   return (
     <>
       {main_image_url && (
-        <div className={css.mapProjectDetailImage}>
-          <img src={fullURL(main_image_url)} alt={`${housing_company}, ${t('SEARCH:project-main-image')}`} />
+        <div className={css.mapProjectDetailImage} aria-hidden="true">
+          <img src={fullURL(main_image_url)} alt="" />
         </div>
       )}
-      <button className={css.closeIcon} onClick={() => onCloseBtnClick()} aria-label={t('SEARCH:hide-project')}>
+      <button
+        className={css.closeIcon}
+        onClick={() => onCloseBtnClick()}
+        aria-label={t('SEARCH:aria-hide-project-popup')}
+      >
         <IconCross aria-hidden="true" />
       </button>
       <div className={css.labelWrap}>
-        <Label type={ownership_type}>{ownership_type}</Label>
+        <Label type={ownership_type}>
+          <>
+            <span className="sr-only">{t('SEARCH:aria-ownership-type')}</span> {ownership_type}
+          </>
+        </Label>
       </div>
       <div className={css.projectDetails}>
+        <span className="sr-only">{t('SEARCH:aria-state-of-sale')}, </span>
         {t(`ES:${state_of_sale}`)}
         <h3>{housing_company}</h3>
       </div>
@@ -54,6 +65,7 @@ const MapProjectPopupCard = ({
             className={`${css.getToKnowButton} hds-button hds-button--secondary hds-button--small`}
           >
             {t('SEARCH:learn-more')}
+            <span className="sr-only">, {housing_company}</span>
           </a>
         )}
         {!hideApartments && hasApartments && (
@@ -62,6 +74,9 @@ const MapProjectPopupCard = ({
             onClick={() => onApartmentsBtnClick()}
             variant="supplementary"
             iconRight={<IconArrowDown aria-hidden="true" />}
+            aria-haspopup="true"
+            aria-controls={`map-project-popup-${id}`}
+            aria-expanded={activeProject?.id === id ? true : false}
           >
             <span className="sr-only">{t('SEARCH:show-all')}</span> {filteredApartments.length} {t('SEARCH:apartments')}
           </Button>
