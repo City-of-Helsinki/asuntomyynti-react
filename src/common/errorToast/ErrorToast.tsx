1f480b04d2d40c09ed7792f67ff64e192595df13
Juha Kujala <juha.kujala@anders.fi>
2021-09-01 14:48:50 +0300

    Add customization options for ErrorToast

--- src/common/errorToast/ErrorToast.tsx
+++ src/common/errorToast/ErrorToast.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { Notification } from 'hds-react';
 import { useTranslation } from 'react-i18next';
 
-const ErrorToast = () => {
+type Props = {
+  autoClose?: boolean | undefined;
+  dismissible?: boolean | undefined;
+  showInline?: boolean | undefined;
+  errorMessage?: string | undefined;
+};
+
+const ErrorToast = ({ autoClose = true, dismissible = false, showInline = false, errorMessage }: Props) => {
   const { t } = useTranslation();
 
   return (
-    <Notification label={t('SEARCH:error')} position="top-right" autoClose type="error">
-      {t('SEARCH:something-wrong')}
+    <Notification
+      label={t('SEARCH:error')}
+      type="error"
+      position={showInline ? 'inline' : 'top-right'}
+      autoClose={autoClose}
+      dismissible={dismissible}
+      closeButtonLabelText={'SEARCH:aria-close-notification'}
+    >
+      {errorMessage ? errorMessage : t('SEARCH:something-wrong')}
     </Notification>
   );
 };
